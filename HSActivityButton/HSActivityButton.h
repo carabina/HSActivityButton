09fb8b782e89c88a60ba52c4475a7590e4834e01
Hussaan S <mhs@quantumcph.com>
2017-11-08 10:32:52 +0500

    Initial Commit

---
+++ HSActivityButton/HSActivityButton.h
@@ -0,0 +1,19 @@
+//
+//  HSActivityButton.h
+//  HSActivityButton
+//
+//  Created by MHS on 08/11/2017.
+//  Copyright © 2017 SST. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HSActivityButton.
+FOUNDATION_EXPORT double HSActivityButtonVersionNumber;
+
+//! Project version string for HSActivityButton.
+FOUNDATION_EXPORT const unsigned char HSActivityButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HSActivityButton/PublicHeader.h>
+
+
